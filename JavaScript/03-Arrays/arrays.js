d71576a71c3f909d29ae66ce5dc5d648adb30855
Ezequiel Moran <moran.margot@hotmail.com>
2022-07-27 16:26:27 -0300

    arrays

--- JavaScript/03-Arrays/arrays.js
+++ JavaScript/03-Arrays/arrays.js
@@ -77,82 +77,6 @@ function barrerArrayConForEach(){
 /* ============================================================================*/
 // OTRAS FORMAS DE UTILIZAR LOS ARRAYS
 
-// ARMAMOS LAS TABLAS DE MULTIPLICACION
-
-// (2)
-function tablaDelDos(){
- let tablaDeDos = [];
-  for(let i = 0; i < 11; i++)
-  {
-    tablaDeDos.push(2 * i);
-  }
-  return tablaDeDos; 
-}
-// (3)
-function tablaDelTres(){
- let tablaDelTres = [];
-  for(let i = 0; i < 11; i++)
-  {
-    tablaDelTres.push(3 * i);
-  }
-  return tablaDelTres; 
-}
-// (4)
-function tablaDelCuatro(){
- let tablaDelCuatro = [];
-  for(let i = 0; i < 11; i++)
-  {
-     tablaDelCuatro.push(4 * i);
-  }
-  return tablaDelCuatro; 
-}
-// (5)
-function tablaDelCinco(){
- let tablaDelCinco = [];
-  for(let i = 0; i < 11; i++)
-  {
-     tablaDelCinco.push(5 * i);
-  }
-  return tablaDelCinco; 
-}
-// (6)
-function tablaDelSeis(){
- let tablaDelSeis = [];
-  for(let i = 0; i < 11; i++)
-  {
-     tablaDelSeis.push(6 * i);
-  }
-  return tablaDelSeis; 
-}
-// 7)
-function tablaDelSiete(){
- let tablaDelSiete = [];
-  for(let i = 0; i < 11; i++)
-  {
-     tablaDelSiete.push(7 * i);
-  }
-  return tablaDelSiete; 
-}
-// 8)
-function tablaDelOcho(){
- let tablaDelOcho = [];
-  for(let i = 0; i < 11; i++)
-  {
-    tablaDelOcho.push(8 * i);
-  }
-  return tablaDelOcho; 
-}
-// (9)
-function tablaDelNueve(){
- let tablaDelNueve = [];
-  for(let i = 0; i < 11; i++)
-  {
-    tablaDelNueve.push(9 * i);
-  }
-  return tablaDelNueve; 
-}
-
-
 // Matriz Mostrar
 function arrayMatriz(){
 
@@ -191,7 +115,7 @@ function ordenarArrayNumeros(){
 }
 
 /*==================================================================================*/
-//FALTA ADAPTAR
+
 function devolverPrimerElemento() {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   let array=["queso",1,2,3,6,5,4,7,8,9];
@@ -271,16 +195,6 @@ function agregarNumeros() {
   } 
   return acu;
 }
-//Promedio
-function ArrayAvg() {
-  let summ = 0;
-  let myArray=[5, 8, 10, 20];
-
-  for (var i = 0;i < myArray.length; i++) {
-      summ = summ + myArray[i];
-  }
-  return summ / myArray.length;
-}
 
 function numeroMasGrande() {
   // Devuelve el número más grande
@@ -289,19 +203,6 @@ function numeroMasGrande() {
   return m;
 }
 
-function multiplicarArgumentos() {
-  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento,
-  // simplemente devuélvelo
-
-  let producto = 1;
-  if (arguments.length === 0){return 0;} 
-  for (let i = 0; i < arguments.length; i ++)
-  {
-      producto = producto * arguments[i];
-  }
-  return producto;
-}
 
 function cuentoElementos(){
   //Realiza una función que retorne la cantidad de los elementos del arreglo
