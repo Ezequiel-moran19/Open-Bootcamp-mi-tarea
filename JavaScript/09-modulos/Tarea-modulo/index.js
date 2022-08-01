355b1326e7aa7440d24a2b6d40a9a8657350f2ea
Ezequiel Moran <moran.margot@hotmail.com>
2022-08-01 14:23:00 -0300

    modulos

---
+++ JavaScript/09-modulos/Tarea-modulo/index.js
@@ -0,0 +1,38 @@
+import * as moduloMatematicas from './modulo/controller.js'
+
+import chalk from 'chalk';
+
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.green('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+const sum = moduloMatematicas.suma(4, 12)
+console.log(sum)
+
+const multi = moduloMatematicas.multiplica(2, 21)
+console.log(multi)
