076f7977397396699ac260d62e6f16c5a7a6b985
Ezequiel Moran <moran.margot@hotmail.com>
2022-12-12 16:08:37 -0300

    primera tarea de C#

---
+++ C#/tarea-1-cSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace tarea_1_cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre" );
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola, " + nombre);
+
+            float hora =  22;
+            Console.WriteLine("Hora, " + hora);
+        }
+    }
+}
